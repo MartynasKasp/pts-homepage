6882fbfcc81dd6da7e5202cd05b371b2d795dab7
Martynas Kasparavičius <martis.kasparavicius@gmail.com>
2019-09-16 14:23:16 +0300

    Social URLs added, links style changed. Header's layout changed.

--- assets/js/layout/Header.js
+++ assets/js/layout/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Header() {
     return (
         <React.Fragment>
-            <div id="profile-picture" className="col-12 col-md-6">
+            <div id="profile-picture" className="col-12 col-md-5">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIHXCzBqH-qb02GHWpaEFTTa6JSOORQQyUOFNTEOR7tONK8smY"
-                     className="rounded-circle center" alt="Profile picture" />
+                     className="rounded-circle" alt="Profile picture" />
             </div>
-            <div id="header-content" className="col-8 col-md-6">
+            <div id="header-content" className="col-8 col-md-7">
                 <div className="row">
                     <div className="col-12">
                         <h1>MARTYNAS KASPARAVIČIUS</h1>
@@ -17,19 +17,19 @@ function Header() {
                     <div className="col-12">
                         <ul className="social">
                             <li>
-                                <i className="fas fa-phone"></i>&nbsp; +370 631 69 663
+                                <i className="fas fa-phone"></i>&nbsp; <a href="#">+370 631 69 663</a>
                             </li>
                             <li>
-                                <i className="fas fa-envelope"></i>&nbsp; martis.kasparavicius@gmail.com
+                                <i className="fas fa-envelope"></i>&nbsp; <a href="#">martis.kasparavicius@gmail.com</a>
                             </li>
                             <li>
-                                <i className="fab fa-linkedin"></i>&nbsp; linkedin.com/in/martynaskaspa
+                                <i className="fab fa-linkedin"></i>&nbsp; <a href="#">linkedin.com/in/martynaskaspa</a>
                             </li>
                             <li>
-                                <i className="fab fa-github"></i>&nbsp; github.com/MartynasKaspa
+                                <i className="fab fa-github"></i>&nbsp; <a href="#">github.com/MartynasKaspa</a>
                             </li>
                             <li>
-                                <i className="fas fa-home"></i>&nbsp; Kaunas, Lietuva
+                                <i className="fas fa-home"></i>&nbsp; <a href="#">Kaunas, Lietuva</a>
                             </li>
                         </ul>
                     </div>
