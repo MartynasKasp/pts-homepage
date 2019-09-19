abe9a12b7c66a57876bec2b66611711b8297f4ce
Martynas Kasparavičius <martis.kasparavicius@gmail.com>
2019-09-19 17:06:53 +0300

    After adding/deleting a social item it shows up/disappears without reloading the page.

--- assets/js/admin_app.js
+++ assets/js/admin_app.js
@@ -9,7 +9,19 @@ require('@fortawesome/fontawesome-free/js/all.js');
 
 class AdminApp extends Component {
 
-    deleteSocialItem(id) {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            socialLinks: window.REP_LOG_APP_PROPS.socialLinks,
+        }
+    }
+
+    deleteSocialItem = (id) => {
+
+        this.setState({ socialLinks:
+            [...this.state.socialLinks.filter(social => social.id !== id)]
+        });
 
         $.ajax({
             url: 'http://127.0.0.1/api/socials/delete',
@@ -25,15 +37,45 @@ class AdminApp extends Component {
                 console.log(`An error occurred: ${xhr.status} ${xhr.statusText}`);
             }
         });
-    }
+    };
+
+    addSocialItem = (social) => {
+
+        this.setState({
+            socialLinks: [...this.state.socialLinks, {
+                name: social.nameValue,
+                url: social.urlValue,
+                icon: social.iconValue
+            }]
+        });
+
+        $.ajax({
+            url: 'http://127.0.0.1/api/socials/add',
+            type: 'POST',
+            data: {
+                name: social.nameValue,
+                url: social.urlValue,
+                icon: social.iconValue
+            },
+            dataType: 'json',
+            success: function(response) {
+                console.log(response)
+            },
+            error: function(xhr) {
+                console.log(`An error occurred: ${xhr.status} ${xhr.statusText}`)
+            }
+        });
+    };
 
     render() {
         return (
             <div className="container">
                 <Navbar />
-                <AddSocialLink />
+                <AddSocialLink
+                    addSocial={this.addSocialItem}
+                />
                 <SocialLinks
-                    socialLinks={window.REP_LOG_APP_PROPS.socialLinks}
+                    socialLinks={this.state.socialLinks}
                     deleteSocial={this.deleteSocialItem}
                 />
             </div>
--- assets/js/layout/admin/AddSocialLink.js
+++ assets/js/layout/admin/AddSocialLink.js
@@ -33,33 +33,13 @@ export class AddSocialLink extends Component {
         }
     };
 
-    addSocialLink = () => {
-
-        $.ajax({
-            url: 'http://127.0.0.1/api/socials/add',
-            type: 'POST',
-            data: {
-                name: this.state.nameValue,
-                url: this.state.urlValue,
-                icon: this.state.iconValue
-            },
-            dataType: 'json',
-            success: function(response) {
-                console.log(response)
-            },
-            error: function(xhr) {
-                console.log(`An error occurred: ${xhr.status} ${xhr.statusText}`)
-            }
-        });
-    };
-
     render() {
         return (
             <div>
                 <input type="text" name="name" placeholder="Social link value" value={this.state.nameValue} onChange={this.handleChange} />
                 <input type="text" name="url" placeholder="Social link URL" value={this.state.urlValue} onChange={this.handleChange} />
                 <input type="text" name="icon" placeholder="Social link icon" value={this.state.iconValue} onChange={this.handleChange} />
-                <button onClick={this.addSocialLink.bind(this)}>
+                <button onClick={this.props.addSocial.bind(this, this.state)}>
                     <i className="fas fa-plus-circle fa-lg"></i>
                 </button>
             </div>
