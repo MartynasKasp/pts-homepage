df0b15abee73c9f8644f8eec762afb42d99b36a3
Martynas Kasparavičius <martis.kasparavicius@gmail.com>
2019-09-20 20:04:25 +0300

    Admin back office page style changed.

--- assets/js/layout/admin/AddSocialLink.js
+++ assets/js/layout/admin/AddSocialLink.js
@@ -46,13 +46,31 @@ export class AddSocialLink extends Component {
 
     render() {
         return (
-            <div>
-                <input type="text" name="name" placeholder="Social link value" style={this.getStyle(this.props.validationErrors.nameError)} value={this.state.nameValue} onChange={this.handleChange} />
-                <input type="text" name="url" placeholder="Social link URL" style={this.getStyle(this.props.validationErrors.urlError)} value={this.state.urlValue} onChange={this.handleChange} />
-                <input type="text" name="icon" placeholder="Social link icon" style={this.getStyle(this.props.validationErrors.iconError)} value={this.state.iconValue} onChange={this.handleChange} />
-                <button onClick={this.props.addSocialItem.bind(this, this.state)}>
-                    <i className="fas fa-plus-circle fa-lg"></i>
-                </button>
+            <div className="row mt-3 d-flex justify-content-center">
+                <div className="col-12 col-md-10">
+                    <div className="form-row">
+                        <div className="col-12 col-md-3">
+                            <div className="form-group">
+                                <input type="text" className="form-control" style={this.getStyle(this.props.validationErrors.nameError)} value={this.state.nameValue} onChange={this.handleChange} name="name" placeholder="Pavadinimas" />
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-3">
+                            <div className="form-group">
+                                <input type="text" className="form-control" style={this.getStyle(this.props.validationErrors.urlError)} value={this.state.urlValue} onChange={this.handleChange} name="url" placeholder="Nuorodos adresas" />
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-3">
+                            <div className="form-group">
+                                <input type="text" className="form-control" style={this.getStyle(this.props.validationErrors.iconError)} value={this.state.iconValue} onChange={this.handleChange} name="icon" placeholder="Ikona" />
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-3">
+                            <button className="btn btn-success" onClick={this.props.addSocialItem.bind(this, this.state)}>
+                                <i className="fas fa-plus-circle"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
